a83c28a68eabff8742f051a30188dd4d0ae18ceb
Redst <bey@mail.ru>
2024-06-26 11:47:41 +0400

    Удалён блинк

--- src/main/java/net/redst/leonidsartifacts/load/InitItems.java
+++ src/main/java/net/redst/leonidsartifacts/load/InitItems.java
@@ -11,7 +11,6 @@ import net.redst.leonidsartifacts.item.*;
 public class InitItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, LeonidsArtifacts.MODID);
     public static final RegistryObject<Item> RADIANCE = ITEMS.register("radiance", Radiance::new);
-    public static final RegistryObject<Item> BLINK_DAGGER = ITEMS.register("blink_dagger", BlinkDagger::new);
     public static final RegistryObject<Item> SIGMIUM_DUST = ITEMS.register("sigmium_dust", SigmiumDust::new);
     public static final RegistryObject<Item> SIGMIUM_INGOT = ITEMS.register("sigmium_ingot", SigmiumIngot::new);
     public static final RegistryObject<Item> RAW_SIGMIUM = ITEMS.register("raw_sigmium", RawSigmium::new);
--- src/main/java/net/redst/leonidsartifacts/load/InitTabs.java
+++ src/main/java/net/redst/leonidsartifacts/load/InitTabs.java
@@ -13,7 +13,6 @@ public class InitTabs {
     public static final RegistryObject<CreativeModeTab> LA_TAB = TAB.register("la_tab",
             () -> CreativeModeTab.builder().title(Component.translatable("tabs.leonidsartifacts.la_tab")).icon(() -> new ItemStack(InitBlocks.SIGMIUM_ORE.get())).displayItems((parameters, tabData) -> {
                 tabData.accept(InitItems.RADIANCE.get());
-                tabData.accept(InitItems.BLINK_DAGGER.get());
                 tabData.accept(InitItems.BROOM.get());
             }).build());
     public static void register(IEventBus eventBus) {
