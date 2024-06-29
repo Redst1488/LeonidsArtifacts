1387ddf0e5ba9993da84555306e3852e802efb72
Redst <bey@mail.ru>
2024-06-29 19:04:02 +0400

    блять я рот ебал

--- src/main/java/net/redst/leonidsartifacts/item/Broom.java
+++ src/main/java/net/redst/leonidsartifacts/item/Broom.java
@@ -1,7 +1,9 @@
 package net.redst.leonidsartifacts.item;
 
-import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
@@ -12,15 +14,15 @@ import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.AirBlock;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.phys.Vec3;
 import net.redst.leonidsartifacts.load.InitEffects;
-import net.redst.leonidsartifacts.utils.Dagger;
+import net.redst.leonidsartifacts.network.Variables;
 
 public class Broom extends SwordItem {
-
-    private static int jumpAmount;
-    private static boolean canDoubleJump;
-
     public Broom() {
         super(new Tier() {
             public int getUses() {
@@ -54,48 +56,65 @@ public class Broom extends SwordItem {
     public void inventoryTick(ItemStack itemstack, Level world, Entity entity, int slot, boolean selected) {
         super.inventoryTick(itemstack, world, entity, slot, selected);
 
-        if (entity instanceof LocalPlayer player) {
+        if (entity instanceof Player player) {
             if (selected) {
+
                 if (player.onGround()) {
-                    jumpAmount = 0;
+                    player.getCapability(Variables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+                        capability.jumpAmount = 0;
+                        capability.syncPlayerVariables(player);
+                    });
                 } else {
-                    if (jumpAmount == 0) {
-                        jumpAmount = 1;
-                        canDoubleJump = true;
-                    }
-                    if (player.input.jumping) {
-                        if (!canDoubleJump && jumpAmount < 2) {
-                            player.jumpFromGround();
-                            world.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, entity.getX(), entity.getY(), entity.getZ(), 0, Math.random() * -0.05, 0);
-                            world.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, entity.getX(), entity.getY(), entity.getZ(), 0, Math.random() * -0.05, 0);
-                            world.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, entity.getX(), entity.getY(), entity.getZ(), 0, Math.random() * -0.05, 0);
-                            jumpAmount++;
-                        }
-                        canDoubleJump = true;
-                    } else {
-                        canDoubleJump = false;
+                    if (((player.getCapability(Variables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new Variables.PlayerVariables())).jumpAmount == 0)) {
+                        player.getCapability(Variables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+                            capability.jumpAmount = 1;
+                            capability.syncPlayerVariables(player);
+                        });
+                        player.getCapability(Variables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+                            capability.canDoubleJump = true;
+                            capability.syncPlayerVariables(player);
+                        });
                     }
                 }
-                player.addEffect(new MobEffectInstance(InitEffects.AGILE.get(), 20, 0, true, false));
+                if ((player.getCapability(Variables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new Variables.PlayerVariables())).DoubleJump) {
+                    world.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, entity.getX(), entity.getY(), entity.getZ(), 0, Math.random() * -0.05, 0);
+                    world.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, entity.getX(), entity.getY(), entity.getZ(), 0, Math.random() * -0.05, 0);
+                    world.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, entity.getX(), entity.getY(), entity.getZ(), 0, Math.random() * -0.05, 0);
+                    player.jumpFromGround();
+                    System.out.print("jump message");
+                }
+                player.addEffect(new MobEffectInstance(InitEffects.AGILE.get(), 1, 0, false, false));
             }
-            if(!selected){
+            if (!selected) {
                 player.removeEffect(InitEffects.AGILE.get());
             }
         }
-
     }
-
     @Override
     public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
         ItemStack itemStack = pPlayer.getItemInHand(pUsedHand);
-        pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY()+Math.random(), pPlayer.getZ(), 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY()+Math.random(), pPlayer.getZ(), 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY()+Math.random(), pPlayer.getZ(), 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY()+Math.random()+0.5, pPlayer.getZ(), 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY()+Math.random()+0.5, pPlayer.getZ(), 0, 0, 0);
-        pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY()+Math.random()+0.5, pPlayer.getZ(), 0, 0, 0);
-        Dagger.Blink(pLevel, pPlayer, itemStack);
+
+        int range = 15;
+        if (pLevel instanceof ServerLevel level) {
+            if (pPlayer instanceof ServerPlayer serverPlayer) {
+                pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY() + Math.random(), pPlayer.getZ(), 0, 0, 0);
+                pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY() + Math.random(), pPlayer.getZ(), 0, 0, 0);
+                pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY() + Math.random(), pPlayer.getZ(), 0, 0, 0);
+                pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY() + Math.random() + 0.5, pPlayer.getZ(), 0, 0, 0);
+                pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY() + Math.random() + 0.5, pPlayer.getZ(), 0, 0, 0);
+                pLevel.addParticle(ParticleTypes.CAMPFIRE_COSY_SMOKE, pPlayer.getX(), pPlayer.getY() + Math.random() + 0.5, pPlayer.getZ(), 0, 0, 0);
+                pPlayer.getCooldowns().addCooldown(pPlayer.getMainHandItem().getItem(), 160);
+
+                for (int i = range; i > 0; --i) {
+                    BlockPos pose = pPlayer.level().clip(new ClipContext(pPlayer.getEyePosition(1f), pPlayer.getEyePosition(1f).add(pPlayer.getViewVector(1f).scale(i)), ClipContext.Block.OUTLINE, ClipContext.Fluid.NONE, pPlayer)).getBlockPos();
+                    Block target = level.getBlockState(pose).getBlock();
+                    if (target instanceof AirBlock) {
+                        serverPlayer.connection.teleport(pose.getCenter().x(), pose.getCenter().y(), pose.getCenter().z(), pPlayer.getYRot(), pPlayer.getXRot());
+                        break;
+                    }
+                }
+            }
+        }
         return InteractionResultHolder.pass(pPlayer.getItemInHand(pUsedHand));
     }
-
 }
