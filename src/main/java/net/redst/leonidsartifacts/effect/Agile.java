9f72905fdac323b4d8a9f896351a6bcd4791e3c5
Redst <bey@mail.ru>
2024-06-29 19:03:49 +0400

    нихуя не починил

--- src/main/java/net/redst/leonidsartifacts/effect/Agile.java
+++ src/main/java/net/redst/leonidsartifacts/effect/Agile.java
@@ -5,14 +5,19 @@ import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
 
 public class Agile extends MobEffect {
 
     public Agile() {
         super(MobEffectCategory.BENEFICIAL, -1);
     }
-
-    public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier){
-        pLivingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1, pAmplifier));
+    //@Override
+    //public void applyEffectTick(LivingEntity pLivingEntity, int pAmplifier){
+       //pLivingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1, 1));
+    //}
+    @Override
+    public void addAttributeModifiers(LivingEntity entity, AttributeMap attributeMap, int amplifier) {
+        entity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1, 1));
     }
 }
